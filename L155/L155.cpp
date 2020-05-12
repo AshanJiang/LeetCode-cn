08e98f143811103c896efbb4b3df329a3036e6df
AshanJiang <ashanjiang@foxmail.com>
2020-05-13 07:32:53 +0800

    添加项目文件。

---
+++ L155/L155.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <stack>
+#include <limits>
+
+using namespace std;
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack <int> s;
+    //用一个辅助栈来保存最小值
+    stack <int> sMin;
+    MinStack() {
+        sMin.push(INT_MAX);
+    }
+
+    void push(int x) {
+        s.push(x);
+        if (x < sMin.top())
+        {
+            sMin.push(x);
+        }
+        else
+        {
+            sMin.push(sMin.top());
+        }
+    }
+
+    void pop() {
+        s.pop();
+        sMin.pop();
+    }
+
+    int top() {
+        return s.top();
+    }
+
+    int getMin() {
+        return sMin.top();
+    }
+};
+
+
+int main()
+{
+	MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    cout << minStack.getMin() << "\n";
+    minStack.pop();
+    cout << minStack.top() << "\n";
+    cout << minStack.getMin() << "\n";
+    return 0;
+}
