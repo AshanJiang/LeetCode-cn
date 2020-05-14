8db627088102136afb2d189fc9630380ed0f5ded
AshanJiang <ashanjiang@foxmail.com>
2020-05-14 08:28:50 +0800

    L136  只出现一次的数字

---
+++ L136/L136.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res{ 0 };
+        for (int x : nums) {
+            res ^= x;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    vector <int> v1{ 4,1,2,1,2 };
+    Solution s;
+    cout << s.singleNumber(v1) << "\n";
+	return 0;
+}
