8a418106db29d5d3019af3e6c3aa1c1aaf395161
AshanJiang <ashanjiang@foxmail.com>
2020-05-22 14:36:44 +0800

    循环解法

--- L105/L105.cpp
+++ L105/L105.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 
@@ -14,17 +15,55 @@ struct TreeNode {
 
 class Solution {
 public:
+    //TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    //    if (preorder.size() == 0)
+    //        return NULL;
+    //    TreeNode* root = new TreeNode(preorder[0]);
+    //    int rootId = find(inorder.begin(), inorder.end(), preorder[0]) - inorder.begin(); //这里可以用hash map加速
+    //    vector<int> leftInorder(inorder.begin(), inorder.begin() + rootId);
+    //    vector<int> rightInorder(inorder.begin() + rootId + 1, inorder.end());
+    //    vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + leftInorder.size() + 1);
+    //    vector<int> rightPreorder(preorder.begin() + leftInorder.size() + 1, preorder.end());
+    //    root->left = buildTree(leftPreorder, leftInorder);
+    //    root->right = buildTree(rightPreorder, rightInorder);
+    //    return root;
+    //}
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         if (preorder.size() == 0)
             return NULL;
+        //循环解法
+        int index = 0; //指向当前节点不断左走的终点
+        stack <TreeNode*> stack; //当前节点的所有还没有考虑过右儿子的祖先节点
         TreeNode* root = new TreeNode(preorder[0]);
-        int rootId = find(inorder.begin(), inorder.end(), preorder[0]) - inorder.begin();
-        vector<int> leftInorder(inorder.begin(), inorder.begin() + rootId);
-        vector<int> rightInorder(inorder.begin() + rootId + 1, inorder.end());
-        vector<int> leftPreorder(preorder.begin() + 1, preorder.begin() + leftInorder.size() + 1);
-        vector<int> rightPreorder(preorder.begin() + leftInorder.size() + 1, preorder.end());
-        root->left = buildTree(leftPreorder, leftInorder);
-        root->right = buildTree(rightPreorder, rightInorder);
+        stack.push(root);
+        for (int i = 1; i < preorder.size(); i++)
+        {
+            TreeNode* tnode = new TreeNode(preorder[i]);
+			if (preorder[i - 1] == inorder[index])
+            {
+                TreeNode* tmpNode = new TreeNode(-1); //初始化
+                while (true)
+                {
+                    if (stack.empty() || inorder[index] != stack.top()->val) // empty先判，不然会越界
+                    {
+                        tmpNode->right = tnode;
+                        break;
+                    }
+                    else
+                    {
+                        tmpNode = stack.top(); //保留上一轮循环的点
+                        stack.pop();
+                    }
+                    index++;
+                }
+            }
+            else
+            {
+                stack.top()->left = tnode;
+            }
+            stack.push(tnode);
+        }
         return root;
     }
 };
@@ -33,8 +72,8 @@ int main()
 {
     Solution s;
     vector <int> preorder, inorder;
-    preorder = { 1,2,4,3,5,7,6,8 };
-    inorder = { 2,4,1,7,5,3,6,8 };
+    preorder = { 3,9,8,5,4,10,20,15,7 };
+    inorder = { 4,5,8,10,9,3,15,20,7 };
     TreeNode* root = s.buildTree(preorder, inorder);
     return 0;
 }
