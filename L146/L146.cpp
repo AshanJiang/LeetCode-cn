bc59c9950b414ade58c14540ae546eb873ce84a0
AshanJiang <ashanjiang@foxmail.com>
2020-05-25 08:48:57 +0800

    L146 双向链表

--- L146/L146.cpp
+++ L146/L146.cpp
@@ -3,55 +3,140 @@
 #include <unordered_map>
 using namespace std;
 
+//class LRUCache {
+//public:
+//    int size{ 0 }, cap{ 0 };
+//    vector<int> v;
+//    unordered_map<int, int> umap;
+//    LRUCache(int capacity) {
+//        size = 0;
+//        cap = capacity;
+//    }
+//
+//    int get(int key) {
+//        if (umap.find(key) != umap.end())
+//        {
+//            v.erase(find(v.begin(), v.end(), key));
+//            v.push_back(key);
+//            return umap[key];
+//        }
+//        else
+//        {
+//            return -1;
+//        }
+//    }
+//
+//    void put(int key, int value) {
+//        if (umap.find(key) != umap.end())
+//        {
+//            umap[key] = value;
+//            v.erase(find(v.begin(), v.end(), key));
+//            v.push_back(key);
+//        }
+//        else
+//        {
+//            if (size >= cap)
+//            {
+//                int deleteKey = v[0];
+//                v.erase(v.begin());
+//                v.push_back(key);
+//                umap.erase(deleteKey);
+//                umap[key] = value;
+//            }
+//            else
+//            {
+//                size++;
+//                v.push_back(key);
+//                umap[key] = value;
+//            }
+//        }
+//    }
+//};
+
+struct DLinkedNode {
+    int key, value;
+    DLinkedNode* prev;
+    DLinkedNode* next;
+    DLinkedNode() : key(0), value(0), prev(nullptr), next(nullptr) {}
+    DLinkedNode(int _key, int _value) : key(_key), value(_value), prev(nullptr), next(nullptr) {}
+};
+
 class LRUCache {
+private:
+    unordered_map<int, DLinkedNode*> cache;
+    DLinkedNode* head;
+    DLinkedNode* tail;
+    int size;
+    int capacity;
+
 public:
-    int size{ 0 }, cap{ 0 };
-    vector<int> v;
-    unordered_map<int, int> umap;
-    LRUCache(int capacity) {
-        size = 0;
-        cap = capacity;
+    LRUCache(int _capacity) : capacity(_capacity), size(0) {
+        // 使用伪头部和伪尾部节点
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
     }
 
     int get(int key) {
-        if (umap.find(key) != umap.end())
-        {
-            v.erase(find(v.begin(), v.end(), key));
-            v.push_back(key);
-            return umap[key];
-        }
-        else
-        {
+        if (!cache.count(key)) {
             return -1;
         }
+        // 如果 key 存在，先通过哈希表定位，再移到头部
+        DLinkedNode* node = cache[key];
+        moveToHead(node);
+        return node->value;
     }
 
     void put(int key, int value) {
-        if (umap.find(key) != umap.end())
-        {
-            umap[key] = value;
-            v.erase(find(v.begin(), v.end(), key));
-            v.push_back(key);
+        if (!cache.count(key)) {
+            // 如果 key 不存在，创建一个新的节点
+            DLinkedNode* node = new DLinkedNode(key, value);
+            // 添加进哈希表
+            cache[key] = node;
+            // 添加至双向链表的头部
+            addToHead(node);
+            ++size;
+            if (size > capacity) {
+                // 如果超出容量，删除双向链表的尾部节点
+                DLinkedNode* removed = removeTail();
+                // 删除哈希表中对应的项
+                cache.erase(removed->key);
+                --size;
+            }
         }
-        else
-        {
-            if (size >= cap)
-            {
-                int deleteKey = v[0];
-                v.erase(v.begin());
-                v.push_back(key);
-                umap.erase(deleteKey);
-                umap[key] = value;
-            }
-            else
-            {
-                size++;
-                v.push_back(key);
-                umap[key] = value;
-            }
+        else {
+            // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
+            DLinkedNode* node = cache[key];
+            node->value = value;
+            moveToHead(node);
         }
     }
+
+    void addToHead(DLinkedNode* node) {
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void removeNode(DLinkedNode* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToHead(DLinkedNode* node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    DLinkedNode* removeTail() {
+        DLinkedNode* node = tail->prev;
+        removeNode(node);
+        return node;
+    }
 };
+
 
 int main()
 {
