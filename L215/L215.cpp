bd9405a3614ed2055e7cf205cf47b879a7b32565
AshanJiang <ashanjiang@foxmail.com>
2020-06-29 15:32:32 +0800

    L215

---
+++ L215/L215.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int findKthLargest(vector<int>& nums, int k) {
+		sort(nums.begin(), nums.end());
+		return nums[nums.size() - k];
+	}
+};
+
+int main()
+{
+	vector <int> nums = { 3,2,3,1,2,4,5,5,6 };
+	int k = 4;
+	Solution ans;
+	cout << ans.findKthLargest(nums, k);
+	return 0;
+}
