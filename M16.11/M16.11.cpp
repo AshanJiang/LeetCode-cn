6c8961157b91e06c438412342a28f18426c28045
AshanJiang <ashanjiang@foxmail.com>
2020-07-08 00:41:58 +0800

    M16.11

---
+++ M16.11/M16.11.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> divingBoard(int shorter, int longer, int k) {
+        set<int> uset;
+        for (int s = k; s >= 0; s--)
+        {
+            int l = k - s;
+            uset.insert(s * shorter + l * longer);
+        }
+        vector<int> res;
+        if (k == 0)
+            return res;
+        copy(uset.begin(), uset.end(), back_inserter(res));
+        return res;
+    }
+};
+
+int main()
+{
+    Solution ans;
+    ans.divingBoard(1, 2, 3);
+    return 0;
+}
