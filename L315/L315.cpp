93d52eeb6332828e30132283a5ea6e1ea9901da9
AshanJiang <ashanjiang@foxmail.com>
2020-07-11 17:17:20 +0800

    L315 框架

---
+++ L315/L315.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+
+    }
+};
+
+int main()
+{
+    Solution ans;
+    vector <int> nums = { 5, 2, 6, 1 };
+    ans.countSmaller(nums);
+	return 0;
+}
