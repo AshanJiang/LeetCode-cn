fd1d43f2f10a851acc2bc39f6ed40fa5c694fbc4
AshanJiang <ashanjiang@foxmail.com>
2020-07-14 10:22:39 +0800

    L120

---
+++ L120/L120.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        size_t n = triangle.size();
+        vector<int> dp(n);
+        for (size_t j = 0; j < n; j++)
+        {
+            dp[j] = triangle[n - 1][j];
+        }
+        for (size_t i = n - 1; i > 0; i--)
+        {
+            for (size_t j = 0; j < i; j++)
+            {
+                dp[j] = triangle[i - 1][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+};
+
+int main()
+{
+    Solution ans;
+    vector<vector<int>> triangle = { {} };
+    cout << ans.minimumTotal(triangle);
+	return 0;
+}
